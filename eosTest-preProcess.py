e8e2fa9ce3b199cdadad9cfc9ff9c7e762764a98
michaelcapizzi <mcapizzi@email.arizona.edu>
2016-04-26 14:19:40 -0700

    added parameter for number of layers

--- eosTest-preProcess.py
+++ eosTest-preProcess.py
@@ -8,17 +8,18 @@ import sys
 #sys.argv[2] = number of lines to take from file
 #sys.argv[3] = word2vec file
 #sys.argv[4] = max length of sentence to consider (currently using 30); needed to handle fixed size of input matrix
-#sys.argv[5] = c_size
-#sys.argv[6] = # of epochs
-#sys.argv[7] = loss function
-#sys.argv[8] = optimizer
-#sys.argv[9] = pickle file for training data
-#sys.argv[10] = pickle file for testing data
+#sys.argv[5] = number of layers
+#sys.argv[6] = c_size
+#sys.argv[7] = # of epochs
+#sys.argv[8] = loss function
+#sys.argv[9] = optimizer
+#sys.argv[10] = pickle file for training data
+#sys.argv[11] = pickle file for testing data
 
 print("loading embeddings")
 w2v = g.Word2Vec.load_word2vec_format(sys.argv[3], binary=False)
 
-model = m.LSTM_keras(embeddingClass=w2v, w2vDimension=int(len(w2v["the"])), max_seq_length=int(sys.argv[4]), cSize=int(sys.argv[5]), num_epochs=int(sys.argv[6]), loss_function=sys.argv[7], optimizer=sys.argv[8])
+model = m.LSTM_keras(num_layers=sys.argv[5], embeddingClass=w2v, w2vDimension=int(len(w2v["the"])), max_seq_length=int(sys.argv[4]), cSize=int(sys.argv[6]), num_epochs=int(sys.argv[7]), loss_function=sys.argv[8], optimizer=sys.argv[9])
 
 print("preparing data file")
 model.prepareData(sys.argv[1], int(sys.argv[2]))
@@ -32,19 +33,20 @@ print("testing")
 model.test_eos()
 
 print("pickling training data")
-model.pickleData(model.training_vectors, sys.argv[9])
+model.pickleData(model.training_vectors, sys.argv[10])
 
 print("pickling testing data")
-model.pickleData(model.testing_vectors, sys.argv[10])
+model.pickleData(model.testing_vectors, sys.argv[11])
 
 print("hyperparameters")
 print("number of lines", sys.argv[2])
 print("word2vec", sys.argv[3])
 print("max sentence length", sys.argv[4])
-print("c_size", sys.argv[5])
-print("number of epochs", sys.argv[6])
-print("loss function", sys.argv[7])
-print("optimizer", sys.argv[8])
+print("number of layers", sys.argv[5])
+print("c_size", sys.argv[6])
+print("number of epochs", sys.argv[7])
+print("loss function", sys.argv[8])
+print("optimizer", sys.argv[9])
 
 # print("saving weights")
 # model.saveWeights("weights.h5")
